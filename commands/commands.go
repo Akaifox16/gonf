5ea582c048b9a0184579c727a5d13ec050f5fb3f
Kamonpat Sunthonpong <kamonpat.sunthonpong@scbtechx.io>
2024-06-10 10:59:14 +0700

    update

--- commands/commands.go
+++ commands/commands.go
@@ -9,23 +9,3 @@ import (
 )
 
 var cfg *config.Config
-
-func CompleteBranch(workflow config.Workflow, branchName string) {
-	runHooks(workflow.PreCommitHooks)
-	fmt.Println("Switching to destination branch and merging changes...")
-	if err := spinner.RunSpinner("push"); err != nil {
-		fmt.Println("Error pushing branch:", err)
-	}
-	runHooks(workflow.PostCommitHooks)
-}
-
-func runHooks(hook string) {
-	if hook == "" {
-		return
-	}
-
-	cmd := exec.Command("sh", "-c", hook)
-	if err := cmd.Run(); err != nil {
-		fmt.Println("Error runing hook:", err)
-	}
-}
